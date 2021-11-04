b2746469c674bdfb1767648cc789af324fd0350e
harishsambasivam <harishsambasivam@gmail.com>
2021-11-05 00:48:24 +0530

    added hashing functions with salt and cost factor with unit tests

--- src/hash/hash.test.js
+++ src/hash/hash.test.js
@@ -3,8 +3,10 @@ const {
   hashWithCostFactor,
   hashWithSalt,
   hash,
-  generateRandomSalt
+  generateRandomSalt,
 } = require("./hashing.js");
+
+const globalDatabase = {};
 
 describe("function: generateRandomSalt", () => {
   it("should generate salt with requested length", () => {
@@ -22,10 +24,65 @@ describe("function: generateRandomSalt", () => {
 });
 
 describe("function: hash", () => {
-  it("should generate hash with requested length", () => {
+  it("should generate hash using sha256", () => {
     const data = "hello world";
     const hashedData = hash(data);
-    console.log(hashedData.length);
+    expect(hashedData).not.toBeNull;
+  });
+
+  it("should return hash of length 128", () => {
+    const data = "hello world";
+    const hashedData = hash(data);
     expect(hashedData.length).toEqual(128);
+  });
+});
+
+describe("function: hashWithSalt", () => {
+  it("should generate hash with adding salt to it", () => {
+    const data = "hello world";
+    const salt = generateRandomSalt(16);
+    const hashedData = hashWithSalt(data, salt);
+    const hashedDataWithOutSalt = hash(data);
+    expect(hashedData).not.toEqual(hashedDataWithOutSalt);
+  });
+
+  it("should return hash of length 128", () => {
+    const data = "hello world";
+    const salt = generateRandomSalt(16);
+    const hashedData = hashWithSalt(data, salt);
+    expect(hashedData.length).toEqual(128);
+  });
+});
+
+describe("function: hashWithCostFactor", () => {
+  it("should generate hash with adding salt and given cost factor", () => {
+    const data = "hello world";
+    const salt = generateRandomSalt(16);
+    const costFactor = 12;
+    const hashedData = hashWithCostFactor(data, salt, costFactor);
+    const hashedDataWithOutSaltRounds = hashWithSalt(data, salt);
+    globalDatabase["data"] = data;
+    globalDatabase["hash"] = hashedData;
+    expect(hashedData).not.toEqual(hashedDataWithOutSaltRounds);
+  });
+
+  it("should return hash of length 128", () => {
+    const data = "hello world";
+    const salt = generateRandomSalt(16);
+    const hashedData = hashWithSalt(data, salt);
+    expect(hashedData.length).toEqual(128);
+  });
+});
+
+describe("function: verifyHash", () => {
+  it("should return true", () => {
+    const data = globalDatabase["data"];
+    const hash = globalDatabase["hash"];
+    expect(verifyHash(data, hash)).toBe(true);
+  });
+  it("should return false", () => {
+    const data = "helloworld!";
+    const hash = globalDatabase["hash"];
+    expect(verifyHash(data, hash)).toBe(false);
   });
 });
--- src/hash/hashing.js
+++ src/hash/hashing.js
@@ -13,7 +13,40 @@ function hash(data) {
     .digest("hex");
 }
 
+// create hash with salt
+function hashWithSalt(data, salt) {
+  data = salt + data;
+  return crypto
+    .createHash("sha512")
+    .update(data)
+    .digest("hex");
+}
+
+// create a hash with cost factor
+function hashWithCostFactor(data, salt, cost) {
+  const costFactor = cost;
+  while (cost >= 1) {
+    data = salt + data;
+    data = crypto
+      .createHash("sha512")
+      .update(data)
+      .digest("hex");
+    cost--;
+  }
+  return `$${costFactor}$${salt}$${data}`;
+}
+
+// verfiy a hash
+function verifyHash(data, hash) {
+  const [rest, costFactor, salt, hashedData] = hash.split("$");
+  const expectedHash = hashWithCostFactor(data, salt, costFactor);
+  return expectedHash == hash;
+}
+
 module.exports = {
+  verifyHash,
+  hashWithCostFactor,
+  hashWithSalt,
   hash,
   generateRandomSalt
 };
