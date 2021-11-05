6d200477f17ce8e8750fc8db58ec2d963d207562
harishsambasivam <harishsambasivam@gmail.com>
2021-11-05 08:31:47 +0530

    added encrypt function for symmetric encryption with unit test

--- src/encryption/encryption.js
+++ src/encryption/encryption.js
@@ -1,5 +1,15 @@
+const crypto = require("crypto");
+
+function symmetricEncryption(message, key) {
+  const randomIV = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes256", key, randomIV);
+  const encryptedMessage =
+    cipher.update(message, "utf-8", "hex") + cipher.final("hex");
+  return encryptedMessage;
+}
+
 function asymmetricEncryption() {
   return 1;
 }
 
-module.exports = { asymmetricEncryption };
+module.exports = { asymmetricEncryption, symmetricEncryption };
--- src/encryption/encryption.test.js
+++ src/encryption/encryption.test.js
@@ -1,4 +1,40 @@
-const { asymmetricEncryption } = require("./encryption");
+const { asymmetricEncryption, symmetricEncryption } = require("./encryption");
+const crypto = require("crypto");
+const globalDatabase = {};
+
+describe("func: symmetricEncryption", () => {
+  it("should return encrypted cipher", () => {
+    const key = crypto.randomBytes(32);
+    const message = "you know what!";
+    const cipherText = symmetricEncryption(message, key);
+    globalDatabase["cipherText"] = cipherText;
+    expect(cipherText.length).toBeGreaterThan(31);
+  });
+
+  it("should not create same cipher text for same input message", () => {
+    const key = crypto.randomBytes(32);
+    const message = "you know what!";
+    const cipherText = symmetricEncryption(message, key);
+    expect(globalDatabase["cipherText"]).not.toEqual(cipherText);
+  });
+});
+
+describe("func: symmetricDecryption", () => {
+  it("should return encrypted cipher", () => {
+    const key = crypto.randomBytes(32);
+    const message = "you know what!";
+    const cipherText = symmetricEncryption(message, key);
+    globalDatabase["cipherText"] = cipherText;
+    expect(cipherText.length).toBeGreaterThan(31);
+  });
+
+  it("should not create same cipher text for same input message", () => {
+    const key = crypto.randomBytes(32);
+    const message = "you know what!";
+    const cipherText = symmetricEncryption(message, key);
+    expect(globalDatabase["cipherText"]).not.toEqual(cipherText);
+  });
+});
 
 describe("func: asymmetricEncryption", () => {
   it("should return something", () => {
